628804bfd62ae8c5826bc3ba12849a42b655068a
MJafar Mashhadi <raindigital2007@gmail.com>
2016-11-02 13:39:28 +0330

    Created models and first migrations

--- instagram/settings.py
+++ instagram/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user_manager.Member'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
--- user_manager/models.py
+++ user_manager/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Member(AbstractUser):
+    phone = models.CharField(max_length=15)  # TODO: add validator
+    bio = models.TextField(max_length=500)
+    profile_picture = models.ImageField()
+
+
+class Follow(TimeStampedModel):
+    who = models.ForeignKey(Member, null=False, related_name='following')
+    whom = models.ForeignKey(Member, null=False, related_name='followers')
+
+
+class Comment(TimeStampedModel):
+    commenter = models.ForeignKey(Member, null=False)
+    media = models.ForeignKey('Media', null=False)
+    comment_text = models.TextField(null=False)
+
+
+class Like(TimeStampedModel):
+    liker = models.ForeignKey(Member, null=False)
+    media = models.ForeignKey('Media', null=False)
+
+
+class HashTag(models.Model):
+    caption = models.CharField(max_length=60)
+
+
+class Media(TimeStampedModel):
+    uploader = models.ForeignKey(Member, null=False, related_name='posts')
+    file = models.FileField(null=False)
+    caption = models.TextField()
+    hash_tags = models.ManyToManyField(HashTag)
+    comments = models.ManyToManyField(Member, through=Comment, related_name='comments')
+    likes = models.ManyToManyField(Member, through=Like, related_name='likes')
