f5f5bc576008699be1c508919db3dc174433b40a
MJafar Mashhadi <raindigital2007@gmail.com>
2016-11-02 18:35:26 +0330

    Added uploaded media to template and views

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
+from user_manager.models import Member, Media
+
 
 def profile(request, username):
-    print('Hello', username)
-
+    current_user = Member.objects.get(username=username)
+    media = Media.objects.filter(uploader=current_user)\
+        .order_by('-created')
     return render(request, 'user_profile/homepage.html', {
-        'username': username
+        'username': username,
+        'user': current_user,
+        'media': media
     })
+
 
 def followers(request, username):
     pass
