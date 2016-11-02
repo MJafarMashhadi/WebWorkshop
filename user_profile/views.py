05ab2ae5df28cd2e892f9887ab2e9337374d1fed
MJafar Mashhadi <raindigital2007@gmail.com>
2016-11-02 15:43:41 +0330

    Fixed templates

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 def profile(request, username):
     print('Hello', username)
-    return render(request, 'user_profile/homepage.html')
+
+    return render(request, 'user_profile/homepage.html', {
+        'username': username
+    })
 
 def followers(request, username):
     pass
