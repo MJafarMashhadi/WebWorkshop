f9dcb8406ad2c14d23f84e771778650b7749dd68
MJafar Mashhadi <raindigital2007@gmail.com>
2016-11-09 14:36:14 +0330

    Decorators

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 
 
+# @login_required
+# from django.views.decorators.http import require_POST, require_GET, require_http_methods
+# @require_http_methods(['GET', "POST", "DELETE"])
 def home(request):
     """
     redirect_url = reverse('profile:profile', kwargs={
